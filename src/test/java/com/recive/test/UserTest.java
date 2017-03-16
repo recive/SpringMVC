b6c32f0172f929ffa7fd8290179108212453bb30
recive <Administrator@USER-BKA4JE2JLR>
2017-03-16 20:14:27 +0800

    Signed-off-by: recive <Administrator@USER-BKA4JE2JLR>

---
+++ src/test/java/com/recive/test/UserTest.java
@@ -0,0 +1,42 @@
+package com.recive.test;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("/config/spring-common.xml")
+public class UserTest {
+
+//	@Autowired
+//	private UserMapper userMapper;
+//	
+//	@Test
+//	public void testAdd(){
+//		User user = new User(-1, "23", "jiuqiyuliang");
+//		userMapper.save(user);
+//		
+//	}
+//	
+//	@Test
+//	public void testFindAll(){
+//		List<User> findAllList = userMapper.findAll();
+//		System.out.println(findAllList.size());
+//	}
+//	
+//	@Test
+//	public void testFindById(){
+//		User user = userMapper.findById(2);
+//		System.out.println(user.getId());
+//		System.out.println(user.getUserName());
+//	}
+//
+//
+//	@Test
+//	public void testUpdate(){
+//		
+//		User user = new User(2, "23", "yuliang");
+//		userMapper.update(user);
+//	}
+//	
+//	@Test
+//	public void testDelete(){
+//		userMapper.delete(1);
+//	}
+}
